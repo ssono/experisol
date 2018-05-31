a30d76efdb487c5b239c968817500f96ce6e0c73
ssono <blusentinel32@gmail.com>
2018-05-31 11:20:26 -0400

    models for stat collection

--- solution/models.py
+++ solution/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
 """
 comments
 modules
@@ -38,3 +39,17 @@ class Section(models.Model):
 
     def __str__(self):
         return self.title
+
+class TotalStats(models.Model):
+    uniqueUsers = models.IntegerField(default=0)
+    numComments = models.IntegerField(default=0)
+    numVotes = models.IntegerField(default=0)
+    totalTime = models.DurationField(default=datetime.timedelta())
+    avgTime = models.DurationField(default=datetime.timedelta())
+
+class UserStats(models.Model):
+    totalStats = models.ForeignKey(TotalStats, on_delete=models.CASCADE, null=True, blank=True, related_name="userStatList")
+    ipHash = models.IntegerField(default=0)
+    timeSpent = models.DurationField(default=datetime.timedelta())
+    comments = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
