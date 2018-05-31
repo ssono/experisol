c2a8059dbd5849ecceac44df9412ce3f4a2f4f01
ssono <blusentinel32@gmail.com>
2018-05-31 12:36:24 -0400

    added stat models to admin

--- solution/admin.py
+++ solution/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from solution.models import Module, Comment, Section
+from solution.models import Module, Comment, Section, TotalStats, UserStats
 from django.contrib import admin
 
 class ModuleAdmin(admin.ModelAdmin):
@@ -13,8 +13,13 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('pk', 'points','module', 'par_comment', 'content')
 
 class TotalStatsAdmin(admin.ModelAdmin):
-    list_display = ()
+    list_display = ('uniqueUsers', 'totalComments', 'totalVotes', 'totalTime', 'avgTime')
+
+class UserStatsAdmin(admin.ModelAdmin):
+    list_display = ('ipHash', 'timeSpent', 'comments', 'votes')
 # Register your models here.
 admin.site.register(Module, ModuleAdmin)
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Section, SectionAdmin)
+admin.site.register(TotalStats, TotalStatsAdmin)
+admin.site.register(UserStats, UserStatsAdmin)
