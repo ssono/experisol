707a280406ef950f87f3a3918cb76fc7aa0bfd55
ssono <blusentinel32@gmail.com>
2018-07-01 13:13:16 -0400

    user model linked to other models

--- solution/admin.py
+++ solution/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from solution.models import Module, Comment, Section, TotalStats, SessionStats, Project
+from solution.models import Module, Comment, Section, TotalStats, SessionStats, Project, Profile
 from django.contrib import admin
 
 class ModuleAdmin(admin.ModelAdmin):
@@ -10,7 +10,7 @@ class SectionAdmin(admin.ModelAdmin):
     list_display = ('title', 'order', 'module')
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'points','module', 'par_comment', 'content')
+    list_display = ('author', 'points','module', 'par_comment', 'content', 'pk')
 
 class TotalStatsAdmin(admin.ModelAdmin):
     list_display = ('uniqueUsers', 'totalComments', 'totalVotes', 'totalTime', 'avgTime')
@@ -20,6 +20,9 @@ class SessionStatsAdmin(admin.ModelAdmin):
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'points', 'pk')
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'joined', 'points', 'email')
 # Register your models here.
 admin.site.register(Module, ModuleAdmin)
 admin.site.register(Comment, CommentAdmin)
@@ -27,3 +30,4 @@ admin.site.register(Section, SectionAdmin)
 admin.site.register(TotalStats, TotalStatsAdmin)
 admin.site.register(SessionStats, SessionStatsAdmin)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- solution/models.py
+++ solution/models.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from datetime import timedelta, time, datetime, date
 """
 comments
@@ -13,9 +17,31 @@ sections
 # Create your models here.
 
 #tite, next/prev_proj, modules, points, author
+class Profile(models.Model):
+    joined = models.DateField(default=date.today)
+    points = models.IntegerField(default=0)
+    bio = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+    #user.profile.stuff
+    #Create Profile on creation of user
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return self.user.username
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
+    authors = models.ManyToManyField(Profile, null=True, blank=True, related_name="projects")
     next_proj = models.OneToOneField('self', on_delete=models.SET_NULL, null=True, blank=True, related_name="prev_proj")
     points = models.IntegerField(default=0)
     created = models.DateField(default=date.today, null=True, blank=True)
@@ -42,6 +68,7 @@ class Comment(models.Model):
     points = models.IntegerField(default=0)
     content = models.TextField()
     created = models.DateField(default=date.today, null=True, blank=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         ordering = ['-points']
