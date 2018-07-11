307c58dfa1fe9fc9273178de997fecc0f5f509e3
ssono <blusentinel32@gmail.com>
2018-07-10 22:09:44 -0400

    working land

--- static/js/intro.js
+++ static/js/intro.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-  $("#enter").hover(function(){
-    $(this).css("background-color", "rgb(80, 204, 127)");
+  $(".tryit").hover(function(){
+    $(this).css("background-color", "rgb(0,242,143)");
     $(this).css("color", "black");
+    $(this).css("border-color", "black");
   }, function(){
     $(this).css("background-color", "rgb(0,220,130)");
     $(this).css("color", "rgb(0,50,80)");
+    $(this).css("border-color", "rgb(0,50,80)");
+
   });
+
+  $(".email_but").hover(function(){
+      $(this).css("background-color", "rgb(0,40,64)");
+      $(this).css("color", "rgb(0,242,143)");
+      $(this).css("border-color", "rgb(0,242,143)");
+    }, function(){
+      $(this).css("background-color", "rgb(0,50,80)");
+      $(this).css("color", "rgb(0,220,130)");
+      $(this).css("border-color", "rgb(0,220,130)");
+    });
 });
